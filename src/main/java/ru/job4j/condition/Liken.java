d4d24545822c11ce8201d51a3e366e9b9f8ee504
KuzmenkoAA <web01proger@mail.ru>
2022-06-25 21:48:56 +0500

    0. Операции сравнения [#235631]

---
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class Liken {
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 9;
+
+        boolean result = first > second;
+        System.out.println(result);
+
+        boolean rsl1 = first < second;
+        System.out.println(rsl1);
+
+        boolean rsl2 = first == second;
+        System.out.println(rsl2);
+    }
+}
