ff1bc2aac0c25741b7db6f8c3e7d2faafc27ea1b
KuzmenkoAA <web01proger@mail.ru>
2022-06-27 16:37:05 +0500

    4.1.2. Глупый бот [#31323 #316498]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -1,20 +1,22 @@
 package ru.job4j.condition;
+
 public class DummyBot {
 
     public static String answer(String question) {
-        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        String rls = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
-            rsl = "Привет, умник.";
+            rls = "Привет, умник.";
         } else if ("Пока.".equals(question)) {
-            rsl = "До скорой встречи.";
+            rls = "До скорой встречи.";
         }
-        return rsl;
+        return rls;
     }
 
     public static void main(String[] args) {
-        String rsl = DummyBot.answer("Привет, Бот.");
-        System.out.println(rsl);
-        rsl = DummyBot.answer("Пока.");
-        System.out.println(rsl);
+        String rls = DummyBot.answer("Привет, Бот.");
+        System.out.println(rls);
+        rls = DummyBot.answer("Пока.");
+        System.out.println(rls);
     }
+
 }
--- src/test/java/ru/job4j/condition/DummyBotTest.java
+++ src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -1,15 +1,16 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Assert;
 
 public class DummyBotTest {
+
     @Test
     public void whenGreetBot() {
         String in = "Привет, Бот.";
         String result = DummyBot.answer(in);
         String expected = "Привет, умник.";
-        Assert.assertEquals(expected, result);
+        Assert.assertEquals(result, expected);
     }
 
     @Test
@@ -17,7 +18,7 @@ public class DummyBotTest {
         String in = "Пока.";
         String result = DummyBot.answer(in);
         String expected = "До скорой встречи.";
-        Assert.assertEquals(expected, result);
+        Assert.assertEquals(result, expected);
     }
 
     @Test
@@ -25,6 +26,6 @@ public class DummyBotTest {
         String in = "Сколько будет 2 + 2?";
         String result = DummyBot.answer(in);
         String expected = "Это ставит меня в тупик. Задайте другой вопрос.";
-        Assert.assertEquals(expected, result);
+        Assert.assertEquals(result, expected);
     }
 }
