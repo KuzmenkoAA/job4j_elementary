a9cacf02eb9abce2ba41f6ef3c4c5034a370c730
KuzmenkoAA <web01proger@mail.ru>
2022-07-03 16:50:50 +0500

    5.0.1. Сумма чисел. [#226416 #318162]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -3,20 +3,17 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        for (; start <= finish; start++) {
-            sum += start;
+        for (int i = start; i <= finish; i++) {
+            sum += i;
         }
         return sum;
     }
 
-    public static int sumByEven(int start, int finish) {
-        int sum = 0;
-        for (; start <= finish; start++) {
-            if (start % 2 == 0) {
-                sum += start;
-            }
-        }
-        return sum;
+    public static void main(String[] args) {
+        System.out.println(sum(0, 10));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
     }
+
 }
 
