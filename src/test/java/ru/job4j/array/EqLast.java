4750eb8c4f9f72d487ba5faa50df1fbe4d041411
KuzmenkoAA <web01proger@mail.ru>
2022-07-27 21:45:38 +0500

    6.3. Массив заполнен true или false [#53857 #325169]

--- src/test/java/ru/job4j/array/EqLast.java
+++ src/test/java/ru/job4j/array/EqLast.java
@@ -2,11 +2,6 @@ package ru.job4j.array;
 
 public class EqLast {
     public static boolean check(int[] left, int[] right) {
-        int tempLeft = left[left.length - 1];
-        int tempRight = right[right.length - 1];
-        if (tempLeft == tempRight) {
-            return true;
-        }
-        return false;
+        return left[left.length - 1] == right[right.length - 1];
     }
 }
